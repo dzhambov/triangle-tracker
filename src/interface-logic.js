7d882898b8137814b47332ecc9821ec841793766
Student <example@example.com>
2019-11-13 13:51:15 -0800

    complete functionality

--- src/interface-logic.js
+++ src/interface-logic.js
@@ -3,3 +3,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+
+$(document).ready(function() {
+  $('#triangle-form').submit(function(event) {
+    event.preventDefault();
+    var sideA = parseInt($("#sideA").val());
+    var sideB = parseInt($("#sideB").val());
+    var sideC = parseInt($("#sideC").val());
+    let myTriangle = new Triangle(sideA, sideB, sideC);
+    $('#output').text(myTriangle.checkType());
+
+  });
+});
