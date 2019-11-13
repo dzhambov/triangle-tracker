dcb794d728d241fe1e4abb708dfa27958490e32d
Student <example@example.com>
2019-11-13 11:49:21 -0800

    add back-end logic to pass tests.

--- src/interface-logic.js
+++ src/interface-logic.js
@@ -1,5 +1,5 @@
 import { Triangle } from './../src/triangle-logic.js';
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
